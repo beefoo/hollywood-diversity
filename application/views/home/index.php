e281f19d6aeba4f0a34b0b1d35bfd938e90a62b5
Brian Foo <brian@youaremyjoy.org>
2015-08-29 17:42:44 -0400

    Homepage draft

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,1 +1,23 @@
-<p>Hello World</p>
+<div class="container home">
+
+  <h2>Help Collect Hollywood Diversity Data</h2>
+
+  <p>
+    Hi, my name is <a href="http://brianfoo.com/">Brian Foo</a> and I am an artist and programmer that is working on a <a href="https://datadrivendj.com/">project that turns data into music</a>. My next song will be made using data from <strong>20 years</strong> of top 50 blockbuster movies. Specifically, I will be looking at the gender and race of each movie's main actors and actresses, producers, directors, and writers. My goal is to research, process, and communicate the diversity (or lack thereof) of mainstream Hollywood movies.
+  </p>
+
+  <p>
+    In order to do this, I compiled a list of <strong>7,000 names</strong> of actors, actresses, producers, directors, and writers from <strong>1,000 movies from 1995-2014</strong>. However, data on these individuals' race (and sometimes gender in the case of production roles) are often not readily available from databases such as <a href="http://www.imdb.com/" target="_blank">IMDB</a>. <strong>That's where you come in</strong>.
+  </p>
+
+  <p>
+    I need help determining the race and gender of the people in this database, which would be very hard to do alone.
+  </p>
+
+  <p>
+    The resulting data will be released freely in the <a href="https://creativecommons.org/publicdomain/mark/1.0/" target="_blank">Public Domain</a> for anyone to use. The data will also be annotated with the sources and a confidence score if it was sourced through this app. It will also be accompanied by a detailed analysis by yours truly.
+  </p>
+
+  <button>Start Classifying Now!</button>
+
+</div>
