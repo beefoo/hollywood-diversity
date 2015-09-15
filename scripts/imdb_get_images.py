6a0c00c57af04b3fbe2d0ecd5567f5039628b42b
beefoo <brian@youaremyjoy.org>
2015-09-15 11:38:55 -0400

    added periodic saving to imdb_get_images, added url read error catching

--- scripts/imdb_get_images.py
+++ scripts/imdb_get_images.py
@@ -18,6 +18,7 @@ if len(sys.argv) < 1:
 PEOPLE_FILE = sys.argv[1]
 overwrite_existing = False
 update_file = True
+save_after = 10
 
 images = {}
 people = []
@@ -44,28 +45,12 @@ with open(PEOPLE_FILE, 'rb') as f:
                 person[h] = row[i]
         people.append(person)
 
-for i, p in enumerate(people):
-    # Image was already found for this person
-    if p['imdb_id'] in images:
-        people[i]['img'] = images[p['imdb_id']]
-    # Otherwise, fetch remote page and parse for image
-    elif overwrite_existing or not p['img']:
-        html_contents = urllib2.urlopen("http://akas.imdb.com/name/nm"+p['imdb_id']+"/").read()
-        contents = BeautifulSoup(html_contents, 'html.parser')
-        image_srcs = contents.findAll('link', rel='image_src')
-        image_src = 'none'
-        # image found
-        if len(image_srcs):
-            image_src = image_srcs[0]['href']
-        # image is default image
-        if 'imdb_fb_logo' in image_src:
-            image_src = 'none'
-        people[i]['img'] = image_src
-        images[p['imdb_id']] = image_src
-        print 'Found ' + people[i]['img'] + ' for '+p['imdb_id']
+def save_people():
+    global PEOPLE_FILE
+    global headers
+    global people
 
-# Write data back to file
-if update_file:
+    # Write data back to file
     with open(PEOPLE_FILE, 'wb') as f:
         w = csv.writer(f)
         w.writerow(headers)
@@ -75,3 +60,35 @@ if update_file:
                 row.append(p[h])
             w.writerow(row)
         print('Successfully updated file: '+PEOPLE_FILE)
+
+unsaved = 0
+for i, p in enumerate(people):
+    save = False
+    # Image was already found for this person
+    if p['imdb_id'] in images:
+        people[i]['img'] = images[p['imdb_id']]
+        unsaved += 1
+    # Otherwise, fetch remote page and parse for image
+    elif overwrite_existing or not p['img']:
+        try:
+            html_contents = urllib2.urlopen("http://akas.imdb.com/name/nm"+p['imdb_id']+"/").read()
+            contents = BeautifulSoup(html_contents, 'html.parser')
+            image_srcs = contents.findAll('link', rel='image_src')
+        except:
+            print("URL Error: " + "http://akas.imdb.com/name/nm"+p['imdb_id']+"/")
+            image_srcs = [{'href': ''}]
+        image_src = 'none'
+        # image found
+        if len(image_srcs):
+            image_src = image_srcs[0]['href']
+        # image is default image
+        if 'imdb_fb_logo' in image_src:
+            image_src = 'none'
+        people[i]['img'] = image_src
+        images[p['imdb_id']] = image_src
+        unsaved += 1
+        print 'Found ' + str(i) + '. ' + people[i]['img'] + ' for '+p['imdb_id']
+    # Save data
+    if update_file and unsaved >= save_after:
+        save_people()
+        unsaved = 0
