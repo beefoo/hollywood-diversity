cf7b701b4c2e029cd1c7577f5f6242ce4944a182
beefoo <brian@youaremyjoy.org>
2015-09-24 12:26:46 -0400

    add race descriptions to guide

--- application/views/pages/guide.php
+++ application/views/pages/guide.php
@@ -21,6 +21,19 @@
 
   <p>Some people are really hard to find information on such as producers or writers. If you are unsure a person's gender, race, or both, just select <em>I am unsure</em> or simply skip them</p>
 
+  <h4>Classifications of race</h4>
+
+  <p>Racial groups are adapted from the <a href="http://quickfacts.census.gov/qfd/meta/long_RHI525214.htm" target="_blank">how the U.S. Census Bureau defined them</a>.</p>
+
+  <ul>
+    <li><strong>White</strong>: A person having origins in any of the original peoples of Europe, the Middle East, or North Africa </li>
+    <li><strong>Black</strong>: A person having origins in any of the Black racial groups of Africa </li>
+    <li><strong>Hispanic (Non-White)</strong>: A person who identifies their origin as Hispanic, Latino, or Spanish</li>
+    <li><strong>Asian/Pacific Islander</strong>: A person having origins in any of the original peoples of the Far East, Southeast Asia, or the Indian subcontinent, or a person having origins in any of the original peoples of Hawaii, Guam, Samoa, or other Pacific Islands</li>
+    <li><strong>American Indian</strong>: A person having origins in any of the original peoples of North and South America and who maintains tribal affiliation or community attachment</li>
+
+  </ul>
+
   <a class="button large" href="/classify">Start Classifying Now!</a>
 
 </div>
