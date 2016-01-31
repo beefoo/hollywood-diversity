b9f3a1f6e7003dc82d0a5f5ea9ce7ea1daaa23bd
beefoo <brian@youaremyjoy.org>
2016-01-31 00:11:38 -0500

    update cast data, update image download script

--- scripts/download_movie_images.py
+++ scripts/download_movie_images.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Example usage:
-#   python download_movie_images.py ../data/people_box_office_top_10_movies_2011-2015_imdb_subset.csv ../tmp/
+#   python download_movie_images.py ../data/cast_box_office_top_10_movies_2006-2015_imdb.csv ../tmp/
 
 import csv
 import os.path
@@ -10,7 +10,7 @@ import urllib2,cookielib
 
 if len(sys.argv) < 2:
     print "Usage: %s <inputfile people csv> <outputdir>" % sys.argv[0]
-    print "       people csv file must contain at least columns: movie_id, imdb_id, img_movie"
+    print "       people csv file must contain at least columns: movie_imdb_id, imdb_id, img_movie"
     sys.exit(1)
 
 INPUT_FILE = sys.argv[1]
@@ -18,7 +18,7 @@ OUTPUT_DIR = sys.argv[2]
 overwrite_existing = False
 
 people = []
-required_cols = ['movie_id', 'imdb_id', 'img_movie']
+required_cols = ['movie_imdb_id', 'imdb_id', 'img_movie']
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -52,11 +52,13 @@ downloaded = 0
 errors = 0
 for p in people:
     extension = 'jpg'
+    if not p['img_movie']:
+        continue
     if '.png' in p['img_movie'] and '.jpg' not in p['img_movie']:
         extension = 'png'
-    filename = OUTPUT_DIR + p['imdb_id'] + '_' + p['movie_id'] + '.' + extension
+    filename = OUTPUT_DIR + p['imdb_id'] + '_' + p['movie_imdb_id'] + '.' + extension
     if overwrite_existing or not os.path.isfile(filename):
-        # print "Downloading " + p['img_movie']
+        print "Downloading " + p['img_movie']
         contents = False
         req = urllib2.Request(p['img_movie'], headers=hdr)
         try:
